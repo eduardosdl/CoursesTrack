745b543c165a0d2ff77ac04f1cab062f4d0571ca
Eduardo Silva <edu.silva.e08@gmail.com>
2024-11-25 21:31:11 -0300

    create register view model and compose screen

---
+++ app/src/main/java/com/eduardosdl/coursestrack/ui/register/RegisterScreen.kt
@@ -0,0 +1,175 @@
+package com.eduardosdl.coursestrack.ui.register
+
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.eduardosdl.coursestrack.R
+import com.eduardosdl.coursestrack.ui.shared.Button
+import com.eduardosdl.coursestrack.ui.shared.OutlinedPasswordField
+import com.eduardosdl.coursestrack.ui.shared.TextField
+import com.eduardosdl.coursestrack.ui.shared.WordMark
+import com.eduardosdl.coursestrack.util.ViewModelState
+
+@Composable
+fun RegisterScreen(
+    viewModel: RegisterViewModel,
+    onRegisterSuccess: () -> Unit,
+    onNavigateToLogin: () -> Unit
+) {
+    val state by viewModel.register.collectAsState()
+
+    RegisterContent(
+        state = state,
+        handleRegister = viewModel::registerUser,
+        onNavigateToLogin = onNavigateToLogin,
+        viewModel = viewModel
+    )
+
+    if (state is ViewModelState.Success) {
+        onRegisterSuccess()
+    }
+
+    if (state is ViewModelState.Failure) {
+        Toast.makeText(
+            LocalContext.current,
+            (state as ViewModelState.Failure).message,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+}
+
+@Composable
+fun RegisterContent(
+    state: ViewModelState<String>,
+    handleRegister: () -> Unit,
+    onNavigateToLogin: () -> Unit,
+    viewModel: RegisterViewModel
+) {
+    val formState by viewModel.formState.collectAsState()
+
+    Scaffold(modifier = Modifier.windowInsetsPadding(WindowInsets.statusBars)) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            WordMark()
+
+            Spacer(modifier = Modifier.height(64.dp))
+
+            TextField(
+                value = formState.email,
+                label = stringResource(R.string.email),
+                isError = formState.emailError != null,
+                onValueChange = { viewModel.onEvent(RegisterViewModel.RegisterEvent.EmailChanged(it)) },
+                errorResource = formState.emailError,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                )
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedPasswordField(
+                value = formState.password,
+                label = stringResource(R.string.password),
+                isError = formState.passwordError != null,
+                onValueChange = {
+                    viewModel.onEvent(
+                        RegisterViewModel.RegisterEvent.PasswordChanged(
+                            it
+                        )
+                    )
+                },
+                errorResource = formState.passwordError,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Next
+                ),
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedPasswordField(
+                value = formState.confirmPassword,
+                label = stringResource(R.string.password_confirm),
+                isError = formState.confirmPasswordError != null,
+                onValueChange = {
+                    viewModel.onEvent(
+                        RegisterViewModel.RegisterEvent.ConfirmPasswordChanged(
+                            it
+                        )
+                    )
+                },
+                errorResource = formState.confirmPasswordError,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Send
+                ),
+                keyboardActions = KeyboardActions(onSend = {
+                    handleRegister()
+                })
+            )
+
+            Spacer(modifier = Modifier.height(64.dp))
+
+            Button(
+                onClick = { handleRegister() },
+                text = stringResource(R.string.create_account),
+                modifier = Modifier.fillMaxWidth(),
+                isLoading = state is ViewModelState.Loading
+            )
+
+
+            Row(
+                modifier = Modifier.padding(vertical = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(R.string.have_account_question),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(
+                    text = stringResource(R.string.login),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.clickable {
+                        onNavigateToLogin()
+                    }
+                )
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/eduardosdl/coursestrack/ui/register/RegisterViewModel.kt
@@ -0,0 +1,114 @@
+package com.eduardosdl.coursestrack.ui.register
+
+import androidx.annotation.StringRes
+import androidx.lifecycle.ViewModel
+import com.eduardosdl.coursestrack.R
+import com.eduardosdl.coursestrack.data.repository.AuthRepository
+import com.eduardosdl.coursestrack.util.ViewModelState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class RegisterViewModel @Inject constructor(private val repository: AuthRepository) : ViewModel() {
+    private val _register = MutableStateFlow<ViewModelState<String>>(ViewModelState.Idle)
+    val register: StateFlow<ViewModelState<String>> = _register
+
+    private val _formState = MutableStateFlow(RegisterState())
+    val formState: StateFlow<RegisterState> = _formState
+
+    fun onEvent(event: RegisterEvent) {
+        when (event) {
+            is RegisterEvent.EmailChanged -> {
+                _formState.value = _formState.value.copy(email = event.email)
+                validateEmail()
+            }
+
+            is RegisterEvent.PasswordChanged -> {
+                _formState.value = _formState.value.copy(password = event.password)
+                validatePassword()
+            }
+
+            is RegisterEvent.ConfirmPasswordChanged -> {
+                _formState.value = _formState.value.copy(confirmPassword = event.confirmPassword)
+                validateConfirmPassword()
+            }
+        }
+    }
+
+    fun registerUser() {
+        if (!validate()) return
+
+        _register.value = ViewModelState.Loading
+        repository.registerUser(
+            _formState.value.email,
+            _formState.value.password,
+            onSuccess = {
+                _register.value = ViewModelState.Success(it)
+            }, onFailure = {
+                _register.value = ViewModelState.Failure(it)
+            })
+    }
+
+    private fun validate(): Boolean {
+        var isValid = true
+
+        if (!validateEmail()) {
+            isValid = false
+        }
+
+        if (!validatePassword()) {
+            isValid = false
+        }
+
+        if (!validateConfirmPassword()) {
+            isValid = false
+        }
+
+        return isValid
+    }
+
+    private fun validateEmail(): Boolean {
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(_formState.value.email).matches()) {
+            _formState.value = _formState.value.copy(emailError = R.string.email_invalid)
+            return false
+        }
+        _formState.value = _formState.value.copy(emailError = null)
+        return true
+    }
+
+    private fun validatePassword(): Boolean {
+        if (_formState.value.password.length < 6) {
+            _formState.value = _formState.value.copy(passwordError = R.string.password_length)
+            return false
+        }
+        _formState.value = _formState.value.copy(passwordError = null)
+        return true
+    }
+
+    private fun validateConfirmPassword(): Boolean {
+        if (_formState.value.confirmPassword.length != _formState.value.password.length) {
+            _formState.value =
+                _formState.value.copy(confirmPasswordError = R.string.password_not_match)
+            return false
+        }
+        _formState.value = _formState.value.copy(confirmPasswordError = null)
+        return true
+    }
+
+    sealed class RegisterEvent {
+        data class EmailChanged(val email: String) : RegisterEvent()
+        data class PasswordChanged(val password: String) : RegisterEvent()
+        data class ConfirmPasswordChanged(val confirmPassword: String) : RegisterEvent()
+    }
+
+    data class RegisterState(
+        val email: String = "",
+        @StringRes val emailError: Int? = null,
+        val password: String = "",
+        @StringRes val passwordError: Int? = null,
+        val confirmPassword: String = "",
+        @StringRes val confirmPasswordError: Int? = null,
+    )
+}
